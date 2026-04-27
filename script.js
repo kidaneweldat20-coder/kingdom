70adc42245ff508c7c55ac91a1f14fb4b6c65bc2
Tofogna <kidaneweldat20@gmail.com>
2026-04-27 10:23:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,6 @@ document.addEventListener('click', function(event) {
 // --- 2. ናይ Backend (Google Sheets + ImgBB) ምስራሕ ---
 const googleSheetUrl = "https://script.google.com/macros/s/AKfycbws9UHPTuGkP0IUic7yAspWYPErqiOkNY49j3ZOuu6UGGEMAg6sv-67bZg8NRq1eTmf/exec";
 const imgbbKey = "470e18bf524a4e7396d4002569e54083";
-
 async function submitBooking(event) {
     event.preventDefault();
     
